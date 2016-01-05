05ee428a0cec86a296d7293d7ec035ff069a45e5
lpeano <peanoluca@gmail.com>
2016-01-05 12:17:42 +0100

    Delete Test

--- test/log/flog.h
+++
@@ -1,5 +0,0 @@
-#include <log_levels.h>
-extern int 			   log_level;
-extern void 	    flog( int , char *, ...);
-extern int 		init_log_level( int  );
-extern int 	               init_logger( char  *);
